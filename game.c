abd0cb40e3b65ca515f97299338c52ff3082226c
Stanley Huang <shuang50@stuy.edu>
2025-01-07 14:50:16 -0500

    end of class.

--- game.c
+++ game.c
@@ -4,16 +4,17 @@
 #include <time.h>
 int game(){
   time_t  begin = time(0);
-  printf("%ld", begin);
   for (int i = 0; i < 1; i++){
     printf("Blue\n");
     char typed[10];
     fgets(typed, 5, stdin);
-    while (strcmp(typed, "Blue") != 0){
+    while (strcmp(typed, "Blue") != 0
       fgets(typed, 5, stdin);
     }
   }
-  return begin;
+  time_t end = time(0);
+  printf("Congratulations! You finished in %ld seconds\n", end - begin);
+  return end - begin;
 }
 int main(){
   game();
