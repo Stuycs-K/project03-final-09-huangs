0e65938195af149bdbbe4741b833687bb300aadb
Stanley huang <shuang50@stuy.edu>
2025-01-09 10:40:25 -0500

    working game

--- game.c
+++ game.c
@@ -8,18 +8,18 @@
 char** randomWords(){
   int file = open("/dev/urandom", O_RDONLY, 0666);
   char ** words = malloc(sizeof(char*) * 10);
-  int * ints = malloc(sizeof(int) * 10);
+  int ints[10];
   for (int i = 0; i < 10; i++){
     int random;
     read(file, &random, sizeof(int));
     random = abs(random) % 100;
     int current = 0;
-    printf("%d\n", random);
+    //printf("%d\n", random);
     while (current != i && ints[current] < random){
       current++;
     }
-    for (int j = i; j > 0; j--){
-      ints[current + j] = ints[current + j - 1];
+    for (int j = 0; j < i - current; j++){
+      ints[i - j] = ints[i - j - 1];
     }
     ints[current] = random;
   }
@@ -31,14 +31,19 @@ char** randomWords(){
   char * wordchars = malloc((int)stat_buffer->st_size);
   read(wordlist, wordchars, (int)stat_buffer->st_size);
   int j = 0;
+  for (int i = 0; i < 10; i++){
+    printf("ints[%d] is %d\n", i, ints[i]);
+  }
   for (int i = 0; i < 100; i++){
     if (j < 10){
       words[j] = strsep(&wordchars, " ");
       if (i == ints[j]){
+        printf("ints[j] is %d\n", ints[j]);
         j++;
       }
     }
   }
+  free(stat_buffer);
   return words;
 }
 
