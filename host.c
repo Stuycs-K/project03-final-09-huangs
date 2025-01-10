bd3dc11f69e7dd5823dba944de358f3ba4b0365a
Stanley huang <shuang50@stuy.edu>
2025-01-10 11:32:51 -0500

    working communication of finish time between players and host

--- host.c
+++ host.c
@@ -9,6 +9,7 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
+#include <signal.h>
 
 union semun {
    int              val;    /* Value for SETVAL */
@@ -18,24 +19,57 @@ union semun {
                             /* (Linux-specific) */
  };
 
+static int KEY = 1867821435;
+static int k0 = 1236432234;
+static int k1 = 1975087341;
+
+static void sighandler(int signo){
+    if (signo == SIGINT){
+        int shmd = shmget(KEY, sizeof(int), IPC_CREAT | 0640);
+        shmctl(shmd, IPC_RMID, 0);
+        printf("siginted\n");
+        exit(0);
+    }
+}
+
 int start(int KEY){
     int shmd = shmget(KEY, sizeof(int), IPC_CREAT | 0640);
     int *start;
     start = shmat(shmd, 0, 0);
     *start = 0;
+
+    int* times[2];
+    int player0 = shmget(k0, sizeof(int), IPC_CREAT | 0640);
+    times[0] = shmat(player0, 0, 0);
+    *times[0] = -1;
+
+    int player1 = shmget(k1, sizeof(int), IPC_CREAT | 0640);
+    times[1] = shmat(player1, 0, 0);
+    *times[1] = -1;
+
     int semd = semget(KEY, 1, IPC_CREAT | 0660);
     union semun us;
     us.val = 1;
     int r = semctl(semd, 0, SETVAL, 2);
+    char buffer[100];
+    fgets(buffer, sizeof(buffer), stdin);
     while (1){
-        char buffer[100];
-        fgets(buffer, sizeof(buffer), stdin);
+        usleep(500000);
+        signal(SIGINT, sighandler);
         if (buffer[strlen(buffer) - 1] == '\n'){
             buffer[strlen(buffer) - 1] = '\0';
         }
-        printf("%d", strcmp(buffer, "start"));
         if (strcmp(buffer, "start") == 0){
             *start = 1;
+        }
+        //printf("times[0] is %d\ntimes[1] is %d\n", *times[0], *times[1]);
+        if (*times[0] != -1){
+            printf("Player 0 has finished in %d seconds\n", *times[0]);
+            *times[0] = -1;
+        }
+        if (*times[1] != -1){
+            printf("Player 1 has finished in %d seconds\n", *times[1]);
+            *times[1] = -1;
         }
     }
 }
--- player.c
+++ player.c
@@ -23,15 +23,28 @@ int connect(int KEY){
   int *data;
   int shmd = shmget(KEY, sizeof(int), IPC_CREAT | 0640);
   data = shmat(shmd, 0, 0);
+
   if (spotsleft == 0){
     printf("Waiting for a player to quit...\n");
+  }
+  int player;
+  int* time;
+  if (spotsleft == 1){
+    int pkey = 1236432234;
+    player = shmget(pkey, sizeof(int), IPC_CREAT | 0640);
+    time = shmat(player, 0, 0);
+  }
+  if (spotsleft == 2){
+    int pkey = 1975087341;
+    player = shmget(pkey, sizeof(int), IPC_CREAT | 0640);
+    time = shmat(player, 0, 0);
   }
   semop(semd, &buffer, 1);
   char bufferr[100];
   while (*data == 0){
     usleep(100000);
   }
-  game();
+  *time = game();
   return 0;
 }
 int main(){
