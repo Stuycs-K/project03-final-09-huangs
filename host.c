9660bc9b2e71747e8156de2794131cf7e7466f10
Stanley huang <shuang50@stuy.edu>
2025-01-10 10:57:47 -0500

    used shared memory segments so player start at same time

--- host.c
+++ host.c
@@ -19,10 +19,25 @@ union semun {
  };
 
 int start(int KEY){
+    int shmd = shmget(KEY, sizeof(int), IPC_CREAT | 0640);
+    int *start;
+    start = shmat(shmd, 0, 0);
+    *start = 0;
     int semd = semget(KEY, 1, IPC_CREAT | 0660);
     union semun us;
     us.val = 1;
     int r = semctl(semd, 0, SETVAL, 2);
+    while (1){
+        char buffer[100];
+        fgets(buffer, sizeof(buffer), stdin);
+        if (buffer[strlen(buffer) - 1] == '\n'){
+            buffer[strlen(buffer) - 1] = '\0';
+        }
+        printf("%d", strcmp(buffer, "start"));
+        if (strcmp(buffer, "start") == 0){
+            *start = 1;
+        }
+    }
 }
 
 int main(){
--- player.c
+++ player.c
@@ -12,17 +12,25 @@
 #include "game.c"
 
 int connect(int KEY){
+  //Semaphore stuff
   struct sembuf buffer;
   buffer.sem_num = 0;
   buffer.sem_flg = SEM_UNDO;
   buffer.sem_op = -1;
   int semd = semget(KEY, 1, 0644);
   int spotsleft = semctl(semd, 0, GETVAL);
+  //Shared memory segments stuff
+  int *data;
+  int shmd = shmget(KEY, sizeof(int), IPC_CREAT | 0640);
+  data = shmat(shmd, 0, 0);
   if (spotsleft == 0){
     printf("Waiting for a player to quit...\n");
   }
   semop(semd, &buffer, 1);
   char bufferr[100];
+  while (*data == 0){
+    usleep(100000);
+  }
   game();
   return 0;
 }
